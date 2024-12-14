958f903f33f2c8bc97735bb3f6e53be1b0dd5175
tjsdn04 <163407812+tjsdn04@users.noreply.github.com>
2024-12-15 04:58:29 +0900

    feat: 캘린더 UI 작업 완료 (#6)

--- ecurator_fe/src/Routes.js
+++ ecurator_fe/src/Routes.js
@@ -3,24 +3,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PostList from "./pages/community/postList";
 import PostWrite from "./pages/community/postWrite";
 import PostView from "./pages/community/postView";
+import Calendar from "./pages/calendar/CalendarPage";
 
 const Router = () => {
-        return (
-        <BrowserRouter>
-            <Routes>
-            {/* 
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 
                             Routes안에 이렇게 작성합니다. 
                             Route에는 react-router-dom에서 지원하는 props들이 있습니다.
     
                             path는 우리가 흔히 말하는 사용하고싶은 "주소"를 넣어주면 됩니다.
                             element는 해당 주소로 이동했을 때 보여주고자 하는 컴포넌트를 넣어줍니다.
                     */}
-            <Route path="/community" element={<PostList />} />
-            <Route path="/postwrite" element={<PostWrite />} />
-            <Route path="/postview" element={<PostView />} />
-            </Routes>
-        </BrowserRouter>
-        );
-    };
-    
-    export default Router;
+        <Route path="/community" element={<PostList />} />
+        <Route path="/postwrite" element={<PostWrite />} />
+        <Route path="/postview" element={<PostView />} />
+        <Route path="/calendar" element={<Calendar />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
